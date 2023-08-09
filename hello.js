3d916007907ef977392ba77cb0f47f91cab448c4
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-08 20:32:08 -0700

    console.log & return

--- hello.js
+++ hello.js
@@ -4,10 +4,18 @@
 
 // sayHello();
 
-const sayHello  = function(name) {
-  console.log("Hello, " + name);
-}
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+// const sayHello  = function(name) {
+//   console.log("Hello, " + name);
+// }
+
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
