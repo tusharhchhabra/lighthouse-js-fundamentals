aed016e883f0e42cb4a45f6442037bf0d1f05f13
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-08-08 22:59:01 -0700

    Iterating over arrays

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+let flavours = iceCreamFlavours;
+flavours.push('root beer');
+
+console.log(flavours);
+console.log(flavours[0]);
+console.log(flavours[flavours.length - 1]);
+console.log(flavours.length);
